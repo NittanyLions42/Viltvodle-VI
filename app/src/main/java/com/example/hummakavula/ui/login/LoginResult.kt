74a1826c4086d561fe880fa87dc00a7cc8afb5b2
zaiverse <brenda.garcia007@mymdc.net>
2023-10-30 22:27:11 -0700

    I added comment in LoginResult class

--- app/src/main/java/com/example/hummakavula/ui/login/LoginResult.kt
+++ app/src/main/java/com/example/hummakavula/ui/login/LoginResult.kt
@@ -3,6 +3,7 @@ package com.example.hummakavula.ui.login
 /**
  * Authentication result : success (user details) or error message.
  */
+// Hi its me brenda
 data class LoginResult(
     val success: LoggedInUserView? = null,
     val error: Int? = null
